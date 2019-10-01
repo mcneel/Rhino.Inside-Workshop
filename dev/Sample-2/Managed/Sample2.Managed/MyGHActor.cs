9a662fe5acb2465b99f452eea0d65f4b24a555b3
Luis Fraguada <luis@mcneel.com>
2019-10-01 19:27:20 +0200

    Basic sample with GH Running Inside...
    Much to cleanup

--- dev/Sample-2/Managed/Sample2.Managed/MyGHActor.cs
+++ dev/Sample-2/Managed/Sample2.Managed/MyGHActor.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
 using UnrealEngine.Engine;
 using UnrealEngine.Runtime;
 
@@ -11,24 +14,80 @@ namespace Sample2
   [UClass, Blueprintable, BlueprintType]
   public class AMyGHActor: AActor
   {
+
+    [UProperty, EditAnywhere, BlueprintReadWrite]
+    public IList<FVector> VertList => null;
+
+    [UProperty, EditAnywhere, BlueprintReadWrite]
+    public IList<int> FaceIDList => null;
+
+
     public override void Initialize(FObjectInitializer initializer)
     {
       base.Initialize(initializer);
+
+    }
+
+    protected override void BeginPlay()
+    {
+      base.BeginPlay();
+    }
+
+
+    [UFunction, BlueprintCallable]
+    public bool CheckMeshes()
+    {
+      Debug.WriteLine("Checking Meshes");
+
+      return RhinoMethods.needsCheck;
+      
     }
 
     [UFunction, BlueprintCallable]
     public void LaunchRhino()
     {
       RhinoMethods.LaunchRhino();
-      RhinoMethods.LaunchGrasshopper();
+    }
+
+    [UFunction, BlueprintCallable]
+    public void CloseRhino()
+    {
+      RhinoMethods.CloseRhino();
     }
 
     [UFunction, BlueprintCallable]
     public void LaunchGrasshopper()
     {
-      
+      RhinoMethods.LaunchGrasshopper();
+    }
+
+    [UFunction, BlueprintCallable]
+    public IList<FVector> GetVertices()
+    {
+      var vertList = RhinoMethods.GetVertices();
+      VertList.Clear();
+      foreach (var v in vertList) VertList.Add(v);
+      return VertList;
+    }
+
+    [UFunction, BlueprintCallable]
+    public IList<int> GetFaceIds()
+    {
+      var faceList = RhinoMethods.GetFaceIds();
+      FaceIDList.Clear();
+      foreach (var i in faceList) FaceIDList.Add(i);
+      return FaceIDList;
+
     }
 
 
+
+
+
+
+
+
+
   }
+
 }
--- dev/Sample-2/Managed/Sample2.Managed/RhinoMethods.cs
+++ dev/Sample-2/Managed/Sample2.Managed/RhinoMethods.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -13,7 +14,7 @@ using UnrealEngine.Runtime;
 
 namespace Sample2
 {
-  class RhinoMethods
+  public class RhinoMethods
   {
     static RhinoCore m_rhino_core;
     static readonly Guid GrasshopperGuid = new Guid(0xB45A29B1, 0x4343, 0x4035, 0x98, 0x9E, 0x04, 0x4E, 0x85, 0x80, 0xD9, 0xCF);
@@ -21,11 +22,11 @@ namespace Sample2
 
     public static Rhino.Geometry.Mesh mesh { get; set; }
 
+    public static bool needsCheck = false;
+    static GH_Document definition;
+
     static RhinoMethods()
     {
-
-      FMessage.Log(ELogVerbosity.Warning, "Hello from C# (Sample2.RhinoMethods:Static Constructor)");
-
       // Resolve RhinoCommon assembly reference
       ResolveEventHandler OnRhinoCommonResolve = null;
       AppDomain.CurrentDomain.AssemblyResolve += OnRhinoCommonResolve = (sender, args) =>
@@ -38,8 +39,9 @@ namespace Sample2
 
         AppDomain.CurrentDomain.AssemblyResolve -= OnRhinoCommonResolve;
 
-        var rhino_system_dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Rhino WIP", "System");
-        return Assembly.LoadFrom(Path.Combine(rhino_system_dir, rhino_common_assembly_name + ".dll"));
+        var rhino_system_dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Rhino 7 WIP", "System");
+        var ass = Assembly.LoadFrom(Path.Combine(rhino_system_dir, rhino_common_assembly_name + ".dll"));
+        return ass;
       };
 
       ResolveEventHandler OnGHResolve = null;
@@ -53,16 +55,17 @@ namespace Sample2
 
         AppDomain.CurrentDomain.AssemblyResolve -= OnGHResolve;
 
-        var gh_plugin_dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Rhino WIP", "Plug-ins", "Grasshopper");
-        return Assembly.LoadFrom(Path.Combine(gh_plugin_dir, gh_assembly_name + ".dll"));
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        var gh_plugin_dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Rhino 7 WIP", "Plug-ins", "Grasshopper");
+
+        var ass = Assembly.LoadFrom(Path.Combine(gh_plugin_dir, gh_assembly_name + ".dll"));
+        return ass;
       };
-
-
     }
 
     public static void LaunchRhino()
     {
-      FMessage.Log(ELogVerbosity.Warning, "Hello from C# (Sample2.RhinoMethods:LaunchRhino)");
 
       // Load Rhino
       if (!m_rhino_init)
@@ -74,10 +77,16 @@ namespace Sample2
         }
         catch (Exception ex)
         {
-          FMessage.Log(ELogVerbosity.Error, "Cannot load Rhino Inside");
-          FMessage.Log(ELogVerbosity.Error, ex.Message);
-          FMessage.Log(ELogVerbosity.Error, ex.Source);
+          Debug.WriteLine(ex.Message);
+          Debug.WriteLine("Cannot load Rhino Inside");
         }
+    }
+
+    public static void CloseRhino()
+    {
+      m_rhino_core.Dispose();
+      m_rhino_core = null;
+      m_rhino_init = false;
     }
 
     public static void LaunchGrasshopper()
@@ -87,11 +96,9 @@ namespace Sample2
 
       PlugIn.LoadPlugIn(GrasshopperGuid);
       Grasshopper.Instances.CanvasCreated += Instances_CanvasCreated;
-      
+
       var ghInit = Rhino.RhinoApp.RunScript("!_-Grasshopper _W _T ENTER", false) ? true : false;
-      
-      //Grasshopper.Instances.ActiveCanvas.Document_ObjectsAdded += Document_ObjectsAdded;
-      //Grasshopper.Instances.ActiveCanvas.DocumentChanged += ActiveCanvas_DocumentChanged;
+
     }
 
     private static void Instances_CanvasCreated(GH_Canvas canvas)
@@ -102,15 +109,115 @@ namespace Sample2
 
     private static void ActiveCanvas_DocumentChanged(GH_Canvas sender, GH_CanvasDocumentChangedEventArgs e)
     {
-      FMessage.Log(ELogVerbosity.Warning, "Hello from C# GH Document Changed");
+      Debug.WriteLine("GH: Document Changed");
+      definition = e.NewDocument;
+      definition.SolutionEnd += Definition_SolutionEnd;
+    }
+
+    private static void Definition_SolutionEnd(object sender, GH_SolutionEventArgs e)
+    {
+      // Process Meshes
+      mesh = GetDocumentPreview(e.Document);
+      if(mesh!=null && mesh.Vertices.Count >0)
+        needsCheck = true;
+      //definition.SolutionEnd -= Definition_SolutionEnd;
+
+
     }
 
     private static void Document_ObjectsAdded(GH_Document sender, GH_DocObjectEventArgs e)
     {
-      FMessage.Log(ELogVerbosity.Warning, "Hello from C# GH Objects Added");
+      Debug.WriteLine("GH: Document Objects Added");
+
     }
 
-    
+    static Rhino.Geometry.Mesh GetDocumentPreview(GH_Document document)
+    {
+      var meshPreview = new Rhino.Geometry.Mesh();
+
+      foreach (var obj in document.Objects.OfType<IGH_ActiveObject>())
+      {
+        if (obj.Locked)
+          continue;
+
+        if (obj is IGH_PreviewObject previewObject)
+        {
+          if (previewObject.IsPreviewCapable)
+          {
+            //primitivesBoundingBox = Rhino.Geometry.BoundingBox.Union(primitivesBoundingBox, previewObject.ClippingBox);
+
+            if (obj is IGH_Component component)
+            {
+              if (!component.Hidden)
+                foreach (var param in component.Params.Output)
+                  foreach (var value in param.VolatileData.AllData(true))
+                  {
+                    if (value is IGH_PreviewData)
+                    {
+                      switch (value.ScriptVariable())
+                      {
+                        case Rhino.Geometry.Mesh mesh:
+                          meshPreview.Append(mesh);
+                          break;
+                        case Rhino.Geometry.Brep brep:
+                          var previewMesh = new Rhino.Geometry.Mesh();
+                          previewMesh.Append(Rhino.Geometry.Mesh.CreateFromBrep(brep, Rhino.Geometry.MeshingParameters.Default));
+                          meshPreview.Append(previewMesh);
+                          break;
+                      }
+                    }
+                  }
+            }
+            else if (obj is IGH_Param param)
+            {
+              foreach (var value in param.VolatileData.AllData(true))
+              {
+                if (value is IGH_PreviewData)
+                {
+                  switch (value.ScriptVariable())
+                  {
+                    case Rhino.Geometry.Mesh mesh:
+                      meshPreview.Append(mesh);
+                      break;
+                    case Rhino.Geometry.Brep brep:
+                      var previewMesh = new Rhino.Geometry.Mesh();
+                      previewMesh.Append(Rhino.Geometry.Mesh.CreateFromBrep(brep, Rhino.Geometry.MeshingParameters.Default));
+                      meshPreview.Append(previewMesh);
+                      break;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+      return meshPreview;
+    }
+
+    public static IList<int> GetFaceIds()
+    {
+      var list = new List<int>();
+      if (mesh != null)
+        foreach (var face in mesh.Faces)
+        {
+          list.Add(face.A);
+          list.Add(face.B);
+          list.Add(face.C);
+
+        }
+      return list;
+    }
+
+    public static IList<FVector> GetVertices()
+    {
+      var list = new List<FVector>();
+      if (mesh != null)
+        foreach (var vert in mesh.Vertices)
+          list.Add(new FVector(vert.X, vert.Y, vert.Z));
+      return list;
+    }
+
 
   }
 }
