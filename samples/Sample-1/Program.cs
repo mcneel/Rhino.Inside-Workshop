160b1ea41c02f0244a4a2616e80bc936b4f36dd9
Kike Garcia <kike.garbo@gmail.com>
2019-10-10 12:39:59 +0200

    Added support for Running Rhino with UI.

--- samples/Sample-1/Program.cs
+++ samples/Sample-1/Program.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Sample1
 {
   class Program
@@ -7,23 +9,39 @@ namespace Sample1
       RhinoInside.Resolver.Initialize();
     }
 
-    static void Main(string[] args)
+    [System.STAThread]
+    static int Main(string[] args)
     {
+      var windowStyle = Rhino.Runtime.InProcess.WindowStyle.NoWindow;
+      if (args.Where(arg => string.Equals(arg, "/WindowStyle=Normal", System.StringComparison.OrdinalIgnoreCase)).Any())
+        windowStyle = Rhino.Runtime.InProcess.WindowStyle.Normal;
+
       try
       {
-        using (new Rhino.Runtime.InProcess.RhinoCore(args))
+        using (var core = new Rhino.Runtime.InProcess.RhinoCore(args, windowStyle))
         {
           var sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 10);
           var mesh = Rhino.Geometry.Mesh.CreateFromSphere(sphere, 10, 10);
           System.Console.WriteLine("Created a mesh with {0} vertices and {1} Faces.", mesh.Vertices.Count, mesh.Faces.Count);
-          System.Console.WriteLine("press any key to exit");
-          System.Console.ReadKey();
+
+          if (windowStyle != Rhino.Runtime.InProcess.WindowStyle.NoWindow)
+          {
+            return core.Run();
+          }
+          else
+          {
+            System.Console.WriteLine("press any key to exit");
+            System.Console.ReadKey();
+            return 0;
+          }
         }
       }
       catch (System.Exception ex)
       {
         System.Console.Error.WriteLine(ex.Message);
       }
+
+      return -1;
     }
   }
 }
