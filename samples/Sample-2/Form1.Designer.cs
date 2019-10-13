23169d830c55d9ccb5f59db5debc5feb5ded84fe
Luis Fraguada <fraguada@gmail.com>
2019-10-13 18:20:50 +0200

    Sample-2

---
+++ samples/Sample-2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Sample_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viewportControl1 = new RhinoWindows.Forms.Controls.ViewportControl();
+            this.SuspendLayout();
+            // 
+            // viewportControl1
+            // 
+            this.viewportControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.viewportControl1.Location = new System.Drawing.Point(12, 12);
+            this.viewportControl1.Name = "viewportControl1";
+            this.viewportControl1.Size = new System.Drawing.Size(883, 767);
+            this.viewportControl1.TabIndex = 0;
+            this.viewportControl1.Text = "viewportControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 791);
+            this.Controls.Add(this.viewportControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RhinoWindows.Forms.Controls.ViewportControl viewportControl1;
+    }
+}
+
---
+++ samples/Sample-2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Rhino.Runtime.InProcess;
+using System;
+using System.Windows.Forms;
+
+namespace Sample_2
+{
+    public partial class Form1 : Form
+    {
+        Rhino.Runtime.InProcess.RhinoCore rhinoCore;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            rhinoCore = new Rhino.Runtime.InProcess.RhinoCore(new string[] {"/NOSPLASH"}, WindowStyle.Hidden, Handle);
+            base.OnHandleCreated(e);
+        }
+
+        protected override void OnHandleDestroyed(EventArgs e)
+        {
+            rhinoCore.Dispose();
+            rhinoCore = null;
+            base.OnHandleDestroyed(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            //Rhino.RhinoDoc.ActiveDoc.Objects.AddSphere(new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 10));
+
+            Rhino.RhinoDoc.Open(@"C:\data\Rhino Logo.3dm", out bool alreadyOpen);
+
+            viewportControl1.Viewport.DisplayMode = Rhino.Display.DisplayModeDescription.FindByName("Arctic");
+            viewportControl1.Invalidate();
+        }
+    }
+}
