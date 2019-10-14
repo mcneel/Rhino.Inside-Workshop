887670f4d82954fe9cde1cef1193155130c44e9f
Luis Fraguada <fraguada@gmail.com>
2019-10-14 10:53:29 +0200

    Adding Sample4 Blueprint

--- samples/Sample-4/Managed/Sample4.Managed/Sample4Actor.cs
+++ samples/Sample-4/Managed/Sample4.Managed/Sample4Actor.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using UnrealEngine.Runtime;
 using UnrealEngine.Engine;
 using Rhino.Runtime.InProcess;
+using Grasshopper.GUI.Canvas;
+using Rhino.PlugIns;
+using Grasshopper.Kernel;
 
 namespace Sample4
 {
@@ -14,6 +17,8 @@ namespace Sample4
   {
 
     static RhinoCore rhinoCore;
+    static readonly Guid GrasshopperGuid = new Guid(0xB45A29B1, 0x4343, 0x4035, 0x98, 0x9E, 0x04, 0x4E, 0x85, 0x80, 0xD9, 0xCF);
+    static GH_Document definition;
 
     static ASample4Actor()
     {
@@ -28,6 +33,37 @@ namespace Sample4
 
       if (rhinoCore == null)
         rhinoCore = new RhinoCore(new string[] { "/NOSPLASH" }, WindowStyle.Hidden);
+    }
+
+    [UFunction, BlueprintCallable]
+    public static void LaunchGrasshopper()
+    {
+      if (!PlugIn.LoadPlugIn(GrasshopperGuid))
+        return;
+
+      Grasshopper.Instances.CanvasCreated += Instances_CanvasCreated;
+
+      var ghInit = Rhino.RhinoApp.RunScript("!_-Grasshopper _W _T ENTER", false) ? true : false;
+
+    }
+
+    private static void Instances_CanvasCreated(GH_Canvas canvas)
+    {
+      Grasshopper.Instances.ActiveCanvas.DocumentChanged += ActiveCanvas_DocumentChanged;
+    }
+
+    private static void ActiveCanvas_DocumentChanged(GH_Canvas sender, GH_CanvasDocumentChangedEventArgs e)
+    {
+      definition = e.NewDocument;
+      definition.SolutionEnd += Definition_SolutionEnd;
+      FMessage.Log(ELogVerbosity.Warning, "Document Changed");
+    }
+
+    private static void Definition_SolutionEnd(object sender, GH_SolutionEventArgs e)
+    {
+      // TODO
+
+      FMessage.Log(ELogVerbosity.Warning, "Solution End");
     }
   }
 }
