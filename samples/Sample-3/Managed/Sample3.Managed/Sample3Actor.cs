d252b772076357202a72cbb5c7bade98b32d3e76
Luis Fraguada <fraguada@gmail.com>
2019-10-14 12:02:14 +0200

    Updating Sample3 to final

--- samples/Sample-3/Managed/Sample3.Managed/Sample3Actor.cs
+++ samples/Sample-3/Managed/Sample3.Managed/Sample3Actor.cs
@@ -1,6 +1,7 @@
 using UnrealEngine.Runtime;
 using UnrealEngine.Engine;
 using Rhino.Runtime.InProcess;
+using System.Collections.Generic;
 
 namespace Sample3
 {
@@ -9,16 +10,15 @@ namespace Sample3
   {
     static RhinoCore rhinoCore;
 
+    [UProperty, EditAnywhere, BlueprintReadWrite]
+    public IList<FVector> VertList => null;
+
+    [UProperty, EditAnywhere, BlueprintReadWrite]
+    public IList<int> FaceIDList => null;
+
     static ASample3Actor()
     {
       RhinoInside.Resolver.Initialize();
-    }
-
-    public override void Initialize(FObjectInitializer initializer)
-    {
-      base.Initialize(initializer);
-      if (rhinoCore == null)
-        rhinoCore = new Rhino.Runtime.InProcess.RhinoCore();
     }
 
     protected override void BeginPlay()
@@ -27,25 +27,54 @@ namespace Sample3
 
       FMessage.Log(ELogVerbosity.Warning, "Hello from C# (" + this.GetType().ToString() + ":BeginPlay)");
 
-      DoSomething();
-
     }
-    public Rhino.Geometry.Mesh mesh;
+
+    [UFunction, BlueprintCallable]
+    public void LaunchRhino()
+    {
+      if (rhinoCore == null)
+        rhinoCore = new RhinoCore(new string[] { "/NOSPLASH" }, WindowStyle.Hidden);
+    }
+
+    [UFunction, BlueprintCallable]
     public void DoSomething()
     {
-      try
-      {
-        var sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 10);
-        mesh = Rhino.Geometry.Mesh.CreateFromSphere(sphere, 10, 10);
-        mesh.Faces.ConvertQuadsToTriangles();
+      var sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 10);
+      var mesh = Rhino.Geometry.Mesh.CreateFromSphere(sphere, 10, 10);
+      mesh.Faces.ConvertQuadsToTriangles();
+      mesh.Flip(true, true, true);
 
-        FMessage.Log(ELogVerbosity.Warning, "Created a mesh with " + mesh.Vertices.Count.ToString() + " vertices and " + mesh.Vertices.Count.ToString() + " Faces.");
-      }
-      catch (System.Exception ex)
+      VertList.Clear();
+      foreach (var vert in mesh.Vertices)
+        VertList.Add(new FVector(vert.X, vert.Y, vert.Z));
+
+      FaceIDList.Clear();
+      foreach (var face in mesh.Faces)
       {
-        FMessage.Log(ELogVerbosity.Error, "Something went wrong with Rhino.Inside.");
-        FMessage.Log(ELogVerbosity.Error, ex.Message);
+        FaceIDList.Add(face.A);
+        FaceIDList.Add(face.B);
+        FaceIDList.Add(face.C);
       }
+
+      FMessage.Log(ELogVerbosity.Warning, "Created a mesh with " + mesh.Vertices.Count.ToString() + " vertices and " + mesh.Vertices.Count.ToString() + " Faces.");
     }
+
+    [UFunction, BlueprintCallable]
+    public IList<FVector> GetVertices()
+    {
+      if (VertList != null)
+        return VertList;
+      return null;
+    }
+
+    [UFunction, BlueprintCallable]
+    public IList<int> GetFaceIds()
+    {
+      if (FaceIDList != null)
+        return FaceIDList;
+      return null;
+    }
+
+
   }
 }
